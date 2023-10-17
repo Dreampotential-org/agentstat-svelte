57d25e05ff0da70c9594bd3176d86278053a8485
unknown <maheechy96@gmail.com>
2023-10-17 22:20:13 +0600

    property card

--- src/components/Card/Card.svelte
+++ src/components/Card/Card.svelte
@@ -25,6 +25,81 @@
   const getImage = `https://maps.googleapis.com/maps/api/streetview?size=400x400&location=${geo_location[1]},${geo_location[0]}&fov=80&heading=70&pitch=0&key=${GOOGLE_KEY}`;
 </script>
 
+<div class="m-5">
+  <div
+    class="mx-auto grid max-w-screen-xl grid-cols-12 space-x-8 overflow-hidden rounded-lg border py-8 text-gray-700 shadow transition hover:shadow-lg"
+  >
+    <a
+      href="#"
+      class="order-2 col-span-3 mt-4 -ml-14 text-left text-gray-600 hover:text-gray-700 sm:-order-1 sm:ml-4"
+    >
+      <div class="group relative h-52 w-52 overflow-hidden rounded-lg">
+        <img
+          src={getImage}
+          alt=""
+          class="h-full w-full object-cover text-gray-700"
+        />
+      </div>
+    </a>
+    <div class="col-span-9 flex flex-col pr-8 text-left sm:pl-4">
+      <h3 class="text-sm text-gray-600">{home_type.replace("_", " ")}</h3>
+      <p p class="mb-3 overflow-hidden pr-7 text-lg font-semibold sm:text-xl">
+        {address_text}
+      </p>
+      <p class="overflow-hidden pr-7 text-sm">
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+        nonummy nibh euismod tincidunt ut laoreet dolore magna .
+      </p>
+
+      <div
+        class="mt-5 flex flex-col space-y-3 text-sm font-medium text-gray-500 sm:flex-row sm:items-center sm:space-y-0 sm:space-x-2"
+      >
+        <div class="">
+          Agent ID : <span class="font-semibold text-black"> {agent_id}</span>
+        </div>
+        <div class="">
+          Name: <span class="font-semibold text-black">{agent_full_name}</span>
+        </div>
+        <div class="">
+          Info: <span class="font-semibold text-black"
+            >{agent_brokerage_info}</span
+          >
+        </div>
+      </div>
+
+      <div
+        class="mt-5 flex flex-col space-y-3 text-sm font-medium text-gray-500 sm:flex-row sm:items-center sm:space-y-0 sm:space-x-2"
+      >
+        <div class="">
+          Status :<span
+            class={`ml-2 mr-3 rounded-full  px-2 py-0.5 ${
+              status === "Sold"
+                ? "text-red-900 bg-red-100"
+                : "text-green-900 bg-green-100"
+            }`}
+          >
+            {status}.
+          </span>
+        </div>
+        <div class="">
+          Price(List/Sold) :<span
+            class="ml-2 mr-3 rounded-full bg-blue-100 px-2 py-0.5 text-blue-900"
+            >${numberWithCommas(list_price_int)}/ ${numberWithCommas(
+              sold_price_int
+            )}</span
+          >
+        </div>
+        <div class="">
+          Sold in :<span
+            class="ml-2 mr-3 rounded-full bg-yellow-100 px-2 py-0.5 text-green-900"
+          >
+            {days_on_market} Days.
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 <div
   style="border-radius: 0.5rem;background-color: rgb(31 41 55);border-color: rgb(55 65 81);display: grid;grid-template-columns: repeat(4, minmax(0, 1fr));gap: 0.25rem;place-items: center; justify-content:center;"
 >
